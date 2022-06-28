ba169aa68053bb98a212c12be40aab7934322bd7
ToxicTerror21 <hussam.baradei@yahoo.com>
2022-06-28 00:56:58 -0400

    Play() now switchs turns +changes button to X | O

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-
-
+let player = 1;
+let turns = 0;
 const modeui = document.getElementById("start");
 const board = document.getElementById("main");
 function gamemode(type){
@@ -10,6 +10,20 @@ function gamemode(type){
 
 
 function play(cord){
-
-
+    turns++;
+    let lookfor = document.getElementById(cord);
+    if(player==1){
+        lookfor.innerText = "X";
+    }else{
+        lookfor.innerHTML ="O";
+    }
+    
+//Switchs Turns After This 
+    if(player==1){
+    player=2;
+    console.log(player);
+}else{
+    player=1;
+    console.log(player);
+}
 }
