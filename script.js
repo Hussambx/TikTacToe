5d2b5a449a6894af8fd4769ebdcf87472455f801
ToxicTerror21 <hussam.baradei@yahoo.com>
2022-06-27 23:50:28 -0400

    Added Play Function to start game

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 
 
 const modeui = document.getElementById("start");
+const board = document.getElementById("main");
 function gamemode(type){
     modeui.style.display = "none";
-    
+    board.style.display = "flex";
+}
+
+
+
+function play(cord){
+
+
 }
