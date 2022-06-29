6f1b0e04bf66c7c417196e591e7e6b97af262730
ToxicTerror21 <hussam.baradei@yahoo.com>
2022-06-29 11:42:26 -0400

    play() checks if grid is already filled by a playe

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let player = 1;
 let turns = 0;
+let gameover = false;
 const modeui = document.getElementById("start");
 const board = document.getElementById("main");
 function gamemode(type){
@@ -10,20 +11,27 @@ function gamemode(type){
 
 
 function play(cord){
-    turns++;
+   
     let lookfor = document.getElementById(cord);
+    if(lookfor.innerText=="" && gameover==false){
+        turns++;
+        if(player==1){
+            lookfor.innerText = "X";
+        }else{
+            lookfor.innerHTML ="O";
+        }
+    //Switchs Turns After This 
     if(player==1){
-        lookfor.innerText = "X";
+        player=2;
+        document.getElementById("turn").innerHTML = "Player 2 Turn"
+        console.log(player);
     }else{
-        lookfor.innerHTML ="O";
+        player=1;
+        console.log(player);
+        document.getElementById("turn").innerHTML = "Player 1 Turn"
     }
-    
-//Switchs Turns After This 
-    if(player==1){
-    player=2;
-    console.log(player);
-}else{
-    player=1;
-    console.log(player);
-}
+    }
+  
+    if(turns==9){}  
+
 }
