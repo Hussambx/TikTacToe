74e6ac5de14fe5ae1889651e79429fe6b6406b89
ToxicTerror21 <hussam.baradei@yahoo.com>
2022-06-29 12:00:24 -0400

    For loop checks horzontially for same symbol

--- script.js
+++ script.js
@@ -22,16 +22,33 @@ function play(cord){
         }
     //Switchs Turns After This 
     if(player==1){
+        conditions("X");
         player=2;
         document.getElementById("turn").innerHTML = "Player 2 Turn"
         console.log(player);
     }else{
+        conditions("O")
         player=1;
         console.log(player);
         document.getElementById("turn").innerHTML = "Player 1 Turn"
     }
+  
     }
   
     if(turns==9){}  
+
+}
+
+function conditions(symbol){
+    for(let ax =0; ax<=6; ax=ax+3){
+        if(document.getElementById(ax).innerText==symbol && document.getElementById(ax+1).innerText==symbol && document.getElementById(ax+2).innerText==symbol){
+            alert("4K");
+            }
+    }
+
+
+
+
+
 
 }
