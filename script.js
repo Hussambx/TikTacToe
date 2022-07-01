04100df926f337c742f0c710827a94c6363fcf71
ToxicTerror21 <hussam.baradei@yahoo.com>
2022-07-01 15:59:00 -0400

    Winning play is green rest of board red

--- script.js
+++ script.js
@@ -38,23 +38,42 @@ function play(cord){
     if(turns==9){}  
 
 }
-
+//If you want add so that the array thats correct gets a color change
 function conditions(symbol){
     for(let ax =0; ax<=6; ax=ax+3){
         if(document.getElementById(ax).innerText==symbol && document.getElementById(ax+1).innerText==symbol && document.getElementById(ax+2).innerText==symbol){
-            alert("4K");
+            gameover = true;
+            document.getElementById(ax).style.backgroundColor = "green";
+            document.getElementById(ax+1).style.backgroundColor ="green";
+            document.getElementById(ax+2).style.backgroundColor = "green";
+            
             }
     }
     for(let ay=0; ay<=2;ay=ay+1){
         if(document.getElementById(ay).innerText==symbol && document.getElementById(ay+3).innerText==symbol && document.getElementById(ay+6).innerText==symbol){
-            alert("4K");
+            gameover = true;
+          
+            document.getElementById(ay).style.backgroundColor = "green";
+            document.getElementById(ay+3).style.backgroundColor ="green";
+            document.getElementById(ay+6).style.backgroundColor = "green";
+            
             }
     }
 
     if(document.getElementById(0).innerText==symbol && document.getElementById(4).innerText==symbol && document.getElementById(8).innerText==symbol){
-        alert("4K");
+        gameover = true;
+        document.getElementById(0).style.backgroundColor = "green";
+            document.getElementById(4).style.backgroundColor ="green";
+            document.getElementById(8).style.backgroundColor = "green";
+           
         }else if(document.getElementById(2).innerText==symbol && document.getElementById(4).innerText==symbol && document.getElementById(6).innerText==symbol){
-            alert("4K");
+            gameover = true;
+            document.getElementById(2).style.backgroundColor = "green";
+            document.getElementById(4).style.backgroundColor ="green";
+            document.getElementById(6).style.backgroundColor = "green";
+           
         }
 
 }
+
+
